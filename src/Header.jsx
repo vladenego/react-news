ef4c11ca37a567d1fd26797b197eb108d1e471b0
vladenego <mistervlad3@gmail.com>
2020-06-13 16:46:59 +0300

    fix(comments): delete extra log

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,23 +1,45 @@
 import React from "react";
 import "./Header.css";
 
-function Header({getNews, componentDidMount }) {
+function Header({
+  getNews,
+  componentDidMount
+}) {
 
-  return (
-    <div>
-      <h1 onClick={() => componentDidMount()} className="nav-title">NEWS</h1>
-      <div className="navigation-wrap">
-        <div className="navigation">
-        <div onClick={() => getNews("business")}><a>Бизнес</a> </div>
-        <div onClick={() => getNews("science")}><a>Наука</a></div>
-        <div onClick={() => getNews("sports")}><a>Спорт</a></div>
-        <div onClick={() => getNews("technology")}><a>Технологии</a></div>
-        {console.log(getNews)}
-        </div> 
-      </div>
-      
-      
-    </div>
+  return ( <
+    div >
+    <
+    h1 onClick = {
+      () => componentDidMount()
+    }
+    className = "nav-title" > NEWS < /h1> <
+    div className = "navigation-wrap" >
+    <
+    div className = "navigation" >
+    <
+    div onClick = {
+      () => getNews("business")
+    } > < a > Бизнес < /a> </div >
+    <
+    div onClick = {
+      () => getNews("science")
+    } > < a > Наука < /a></div >
+    <
+    div onClick = {
+      () => getNews("sports")
+    } > < a > Спорт < /a></div >
+    <
+    div onClick = {
+      () => getNews("technology")
+    } > < a > Технологии < /a></div > {
+
+    } <
+    /div>  < /
+    div >
+
+
+    <
+    /div>
   );
 }
 
